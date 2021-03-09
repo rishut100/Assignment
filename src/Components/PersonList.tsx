43441690fc9004275547c00351c496d6bb16f781
Rishu Tiwari <rishut100@gmail.com>
2021-03-09 17:45:33 +0530

    second commit

---
+++ src/Components/PersonList.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Data} from './Interfaces';
+
+interface PList{
+    items : Data[]
+}
+
+const PersonList: React.FC<PList> =(Props : Array<object>) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PersonList;
--- src/Components/Persons.tsx
+++ src/Components/Persons.tsx
@@ -1,83 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {useState, useEffect} from 'react';
+import { Data, RootObject} from './Interfaces';
 import Axios from 'axios';
+import PersonList from './PersonList';
 
-interface PState{
-    personData?: [];
+const Person : React.FC = () => {
+    const [person, setPerson] = useState<Array<object>>([{id:0,email:'',first_name:'',last_name:'',avatar:''}])
+
+    useEffect( ()=>{
+        let result:RootObject;
+        Axios.get("https://reqres.in/api/users/")
+        .then((response) => {
+            result = response.data;
+            setPerson(result.data);
+        })
+        .catch((err) => {
+            console.log("Error is"+err);
+        })   
+    },[]);    
+
+    return (
+        <div className="container">
+            <PersonList items={person}/>
+        </div>
+    )
 }
 
-export default class Persons extends Component<PState> {
-    constructor(props: PState){
-        super(props);
-
-        this.state ={
-            personData : []
-        }
-
-        this.getData = this.getData.bind(this);
-
-    }
-
-    componentDidMount(){
-        this.getData()
-    }
-    // async gettingData(){
-    //     let response = await Axios.get('https://reqres.in/api/users/');
-    //     let data = response.data;
-    //     console.log(data);
-    //     console.log(56);
-    // }
-
-    async getData(){
-        const resApi = await Axios.get("https://reqres.in/api/users/");
-        this.setState({
-            personData : resApi.data.data
-        });
-        console.log(resApi.data.data);
-        // console.log(this.state.personData);
-        const pData = resApi.data.data;
-        console.log(pData[0]);
-    }
-
-    render() {
-        return (
-            <div className="container mt-5 ">
-                <h1 className="text-lg-dark mt-1">Person Data</h1>
-                <section className="person-data">
-                    <div className="container">
-                        <div className="row justify-content-center">
-                            <div className="col-md-4 col-lg-4">
-                                <div className="card person-data-card">
-                                    <div className="card-body">
-                                        some text here
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-md-4 col-lg-4">
-                                <div className="card person-data-card">
-                                    <div className="card-body">
-                                        some text here
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-md-4 col-lg-4">
-                                <div className="card person-data-card">
-                                    <div className="card-body">
-                                        some text here
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-md-4 col-lg-4">
-                                <div className="card person-data-card">
-                                    <div className="card-body">
-                                        some text here
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </div>
-            
-        )
-    }
-}
+export default Person;
