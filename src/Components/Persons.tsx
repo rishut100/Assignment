f30ed521f3ccc4d1a232a8e52ac69b955ada8e52
Rishu Tiwari <rishut100@gmail.com>
2021-03-11 21:43:46 +0530

    seventh commit

--- src/Components/Persons.tsx
+++ src/Components/Persons.tsx
@@ -9,12 +9,14 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 const Person : React.FC= () => {
     const [person, setPerson] = useState<Array<Data>>([{id:0,email:'',first_name:'',last_name:'',avatar:''}]);
     const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
 
     useEffect( ()=>{
         let result:RootObject;
         Axios.get(`https://reqres.in/api/users?page=${page}`)
         .then((response) => {
             result = response.data;
+            setTotalPages(result.total); 
             if(page === 1 && result.data.length >= 6 ){
                 setPerson(result.data);    
             }
@@ -36,13 +38,14 @@ const Person : React.FC= () => {
         <InfiniteScroll
         dataLength = {person.length}
         next= {() => {setPage(prev => prev + 1)}}
-        hasMore ={value =  page > 12 ? false : true}
+        hasMore ={value =  page > totalPages ? false : true}
         loader = {<h4>Loading...</h4>}
-        endMessage ={
+        endMessage ={ 
             <p style={{textAlign : 'center'}}>
                 <strong>That's all for today !!! </strong>
             </p>
         }
+        scrollThreshold = "100%"
         >
             <div className="container">
                 <PersonList items={person}/>
